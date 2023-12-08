0a7df41adeb5a368cedb3fa4df69c4665e95d597
fredgyoung <fredgyoung@gmail.com>
2023-12-08 17:10:55 -0500

    new stuff

--- script.js
+++ script.js
@@ -13,9 +13,11 @@ function draw() {
 
 // Draw snake
 function drawSnake() {
-    snake.forEach((segmant) => {
+    snake.forEach((segment) => {
         const snakeElement = createGameElement('div', 'snake');
-    })
+        setPosition(snakeElement, segment);
+        board.appendChild(snakeElement);
+    });
 }
 
 // Create a snake or food div
@@ -23,4 +25,10 @@ function createGameElement(tag, className) {
     const element = document.createElement(tag);
     element.className = className;
     return element;
+}
+
+// Set the position of the snake or the food
+function setPosition(element, position) {
+    element.style.gridColumn = position.x;
+    element.style.gridRow = position.y;
 }
